f4b2f22d3dfb73d4d465cf2c9a21746e4c4f5d98
Simon Goffaux <sgoffaux@student.s19.be>
2021-11-19 15:20:02 +0100

    ex00 removed comment

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,15 +6,6 @@ int main(int argc, char **argv)
 {
 	if (argc == 2)
 	{
-		// char *next;
-		// char *next2;
-		// double test = strtod(argv[1], &next);
-		// long int test2 = strtol(argv[1], &next2, 10);
-		// std::cout << "double: " << test << std::endl;
-		// std::cout << "double_next: " << next << std::endl;
-		// std::cout << "long: " << test2 << std::endl;
-		// std::cout << "long_next: " << next2 << std::endl;
-
 		try
 		{
 			Scalar scalar(argv[1]);
