6f3d1ce316112e85069c80d31b20fb8a17f26751
Simon Goffaux <sgoffaux@student.s19.be>
2021-11-25 13:44:18 +0100

    tab size

--- cpp08/ex02/includes/mutantstack.hpp
+++ cpp08/ex02/includes/mutantstack.hpp
@@ -22,8 +22,8 @@ public:
 	typedef typename Container::iterator				iterator;
 	typedef typename Container::reverse_iterator		reverse_iterator;
 
-	iterator				begin() { return this->c.begin(); };
-	iterator				end() { return this->c.end(); };
-	reverse_iterator		rbegin() { return this->c.rbegin(); };
-	reverse_iterator		rend() { return this->c.rend(); };
+	iterator			begin() { return this->c.begin(); };
+	iterator			end() { return this->c.end(); };
+	reverse_iterator	rbegin() { return this->c.rbegin(); };
+	reverse_iterator	rend() { return this->c.rend(); };
 };
