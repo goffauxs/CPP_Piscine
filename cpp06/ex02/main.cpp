bb8b51f93cfe67e10fbe5fdc32d8b0dd4c9d7e1e
Simon Goffaux <sgoffaux@student.s19.be>
2021-12-01 15:20:40 +0100

    add condition to check if all three casts from pointer fail and add try catch block to catch exception if all three casts from reference fail

--- cpp06/ex02/main.cpp
+++ cpp06/ex02/main.cpp
@@ -10,13 +10,13 @@ Base* generate()
 	switch (rand() % 3)
 	{
 	case 0:
-		std::cout << "A" << std::endl;
+		std::cout << "A";
 		return new A();
 	case 1:
-		std::cout << "B" << std::endl;
+		std::cout << "B";
 		return new B();
 	case 2:
-		std::cout << "C" << std::endl;
+		std::cout << "C";
 		return new C();
 	default:
 		return NULL;
@@ -32,25 +32,41 @@ void identify(Base* p)
 		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p))
 		std::cout << "C" << std::endl;
+	else
+		std::cerr << "bad cast" << std::endl;
+
 }
 
 void identify(Base& p)
 {
-	std::cout << "Identify by reference: ";
-	if (dynamic_cast<A*>(&p))
-		std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(&p))
-		std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(&p))
-		std::cout << "C" << std::endl;
+	try
+	{
+		std::cout << "Identify by reference: ";
+		if (dynamic_cast<A*>(&p))
+			std::cout << "A" << std::endl;
+		else if (dynamic_cast<B*>(&p))
+			std::cout << "B" << std::endl;
+		else if (dynamic_cast<C*>(&p))
+			std::cout << "C" << std::endl;
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+	}
 }
 
 int main()
 {
 	srand(time(NULL));
 	Base* ptr = generate();
-	Base& ref = *generate();
+	std::cout << " (pointer)" << std::endl;
+	Base* tmp = generate();
+	Base& ref = *tmp;
+	std::cout << " (reference)" << std::endl;
 
 	identify(ptr);
 	identify(ref);
+
+	delete ptr;
+	delete tmp;
 }
