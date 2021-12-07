ef8f6a7959993a0931ab75a233f4de90534bc616
Simon Goffaux <sgoffaux@student.s19.be>
2021-12-07 10:57:50 +0100

    added tests in main for ex02

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -3,16 +3,18 @@
 
 int main()
 {
+	std::cout << "--- PDF example ---" << std::endl;
+
 	MutantStack<int> mstack;
 
 	mstack.push(5);
 	mstack.push(17);
 
-	std::cout << mstack.top() << std::endl;
+	std::cout << "top: " << mstack.top() << std::endl;
 
 	mstack.pop();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << "size: " << mstack.size() << std::endl;
 
 	mstack.push(3);
 	mstack.push(5);
@@ -30,5 +32,49 @@ int main()
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "--- Reverse iterator ---" << std::endl;
+
+	MutantStack<std::string> rev;
+
+	rev.push("one");
+	rev.push("two");
+	rev.push("three");
+	rev.push("four");
+	rev.push("five");
+
+	MutantStack<std::string>::reverse_iterator rev_itr = rev.rbegin();
+	for (; rev_itr != rev.rend(); rev_itr++)
+		std::cout << *rev_itr << std::endl;
+
+	std::cout << "--- Copy constructor ---" << std::endl;
+
+	MutantStack<int> copy(mstack);
+	MutantStack<int> a_copy = mstack;
+
+	copy.pop();
+	copy.pop();
+	copy.pop();
+	copy.push(64);
+	copy.push(65);
+	copy.push(66);
+
+	MutantStack<int>::iterator copy_itr = copy.begin();
+	for (; copy_itr != copy.end(); copy_itr++)
+		std::cout << *copy_itr << std::endl;
+
+	std::cout << "--- Assignment operator ---" << std::endl;
+
+	a_copy.pop();
+	a_copy.pop();
+	a_copy.pop();
+	a_copy.push(128);
+	a_copy.push(129);
+	a_copy.push(130);
+
+	MutantStack<int>::iterator a_copy_itr = a_copy.begin();
+	for (; a_copy_itr != a_copy.end(); a_copy_itr++)
+		std::cout << *a_copy_itr << std::endl;
+
 	return 0;
 }
