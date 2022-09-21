553638fe645cd3983abab13575e76de9edcdd39f
Simon Goffaux <sgoffaux@student.s19.be>
2022-09-21 14:19:09 +0200

    copy vogsphere changes

--- cpp01/ex00/Zombie.cpp
+++ cpp01/ex00/Zombie.cpp
@@ -13,5 +13,5 @@ Zombie::~Zombie( void )
 
 void Zombie::announce( void ) const
 {
-	std::cout << "<" << this->_name << "> BraiiiiiiinnnzzzZ..." << std::endl;
+	std::cout << this->_name << " BraiiiiiiinnnzzzZ..." << std::endl;
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -14,22 +14,26 @@ int main(int argc, char **argv)
 			"ERROR"
 		};
 		int i = 0;
-		while (levels[i] != argv[1])
+		while (i < 4 && levels[i] != argv[1])
 			i++;
 		switch (i)
 		{
 			case 0:
 				std::cout << "[ " << levels[0] << " ]" << std::endl;
 				karen.complain(levels[0]);
+				std::cout << std::endl;
 			case 1:
 				std::cout << "[ " << levels[1] << " ]" << std::endl;
 				karen.complain(levels[1]);
+				std::cout << std::endl;
 			case 2:
 				std::cout << "[ " << levels[2] << " ]" << std::endl;
 				karen.complain(levels[2]);
+				std::cout << std::endl;
 			case 3:
 				std::cout << "[ " << levels[3] << " ]" << std::endl;
 				karen.complain(levels[3]);
+				std::cout << std::endl;
 				break;
 			default:
 				std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
